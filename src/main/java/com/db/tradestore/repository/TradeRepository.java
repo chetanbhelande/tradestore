8e401eecfb9c6322ec038673bc34fe4774c5a42e
cheta <cheta@LAPTOP-27OQBSUV>
2021-04-23 19:09:18 +0530

    generated Trade Repository now our test class will pass

---
+++ src/main/java/com/db/tradestore/repository/TradeRepository.java
@@ -0,0 +1,15 @@
+package com.db.tradestore.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.db.tradestore.model.Trade;
+import com.db.tradestore.model.TradeId;
+
+@Repository
+public interface TradeRepository extends JpaRepository<Trade,TradeId> {
+//	Trade findFirstByIdOrderByVersionDesc(TradeId id);
+	Optional<Trade> findFirstByTradeIdOrderByVersionDesc(String id);
+}
